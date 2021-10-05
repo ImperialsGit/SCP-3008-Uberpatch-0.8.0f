3c7ee82b5956fc6cb46472be1a997d63e978adb6
Imperial <68413525+ImperialsGit@users.noreply.github.com>
2021-10-05 15:28:06 -0500

    Added a few more mods, added translation controls to the installer

---
+++ CS mods/Development/AllComponents.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+//DELCLHOOKWORLDGENERATED
+public class AllComponents : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(GameManager.s.FPCam.transform.position, GameManager.s.FPCam.transform.forward, out hit, 100))
+            {
+                foreach(Component C in hit.transform.GetComponents<Component>())
+                {
+                    TGLogger.Log(C.ToString());
+                }
+                foreach(Component C in hit.transform.GetComponentsInChildren<Component>())
+                {
+                    TGLogger.Log(C.ToString());
+                }
+            }
+        }
+    }
+}
